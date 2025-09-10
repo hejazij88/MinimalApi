003ee204a39923bf3e7ff5697708f652d5566718
98913 <98913@LAPTOP-JQR66TAF>
2025-09-10 15:57:39 +0330

    Create Services

---
+++ MinimalApi/Services/TodoService.cs
@@ -0,0 +1,32 @@
+﻿using MinimalApi.Models;
+
+namespace MinimalApi.Services;
+
+public class TodoService:ITodoService
+{
+    private readonly List<TodoItem> _items = new();
+    private int _next = 1;
+
+
+    public IEnumerable<TodoItem> GetAll() => _items;
+
+
+    public TodoItem? GetById(int id) => _items.FirstOrDefault(x => x.Id == id);
+
+
+    public TodoItem Create(string title, bool isCompleted)
+    {
+        var t = new TodoItem { Id = _next++, Title = title, IsCompleted = isCompleted, CreatedAt = DateTime.UtcNow };
+        _items.Add(t);
+        return t;
+    }
+
+
+    public bool Delete(int id)
+    {
+        var t = GetById(id);
+        if (t is null) return false;
+        _items.Remove(t);
+        return true;
+    }
+}
