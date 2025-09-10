9b4e59d739a3cee6d3dd6e99831a304e18326e16
98913 <98913@LAPTOP-JQR66TAF>
2025-09-10 15:49:34 +0330

    Create RequestTimingMiddleware

---
+++ MinimalApi/Middlewares/RequestTimingMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace MinimalApi.Middlewares;
+
+public class RequestTimingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RequestTimingMiddleware> _logger;
+
+    public RequestTimingMiddleware(RequestDelegate next, ILogger<RequestTimingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var sw = Stopwatch.StartNew();
+        await _next(context);
+        sw.Stop();
+
+
+        _logger.LogInformation("Request {method} {path} responded {statusCode} in {ms}ms",
+            context.Request.Method, context.Request.Path, context.Response.StatusCode, sw.ElapsedMilliseconds);
+
+
+        if (!context.Response.HasStarted)
+            context.Response.Headers["X-Response-Time-ms"] = sw.ElapsedMilliseconds.ToString();
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
