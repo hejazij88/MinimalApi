2a69029934e017f4cd83724ccd47fd97ea5de2f4
98913 <98913@LAPTOP-JQR66TAF>
2025-09-10 15:55:15 +0330

    Create Dtos

---
+++ MinimalApi/Dtos/TodoDto.cs
@@ -0,0 +1,19 @@
+﻿namespace MinimalApi.Dtos;
+
+public class TodoDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+
+    public TodoDto() { }
+    public TodoDto(Models.TodoItem src)
+    {
+        Id = src.Id;
+        Title = src.Title;
+        IsCompleted = src.IsCompleted;
+        CreatedAt = src.CreatedAt;
+    }
+}
